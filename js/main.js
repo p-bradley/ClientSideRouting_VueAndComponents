d51b793e563d8881861b544c69c1ab2e5d1073b3
p-bradley <p_bradley@fanshaweonline.ca>
2020-02-04 17:43:08 -0500

    finished log in

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
 //create a couple of components we can request and render
-let SplashComponent = {
-    template: `<h1>Welcome to my super awesome app!</h1>`
-};
+import SplashComponent from "./modules/SplashComponent.js";
+import AppComponent from "./modules/AppComponent.js";
+import ErrorComponent from "./modules/ErrorComponent.js";
 
-let AppComponent = {
-    template: `<h1>This is the app page</h1>`
-};
-
-let ErrorComponent = {
-    template: `<h1>That page doesn't exist!</h1>`
-}
 // these are the same express routes --> router.get('/', ... do something with the request)
 const routes = [
     { path: '/', name: 'splash', component: SplashComponent },
